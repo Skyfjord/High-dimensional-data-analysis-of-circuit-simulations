0863c373827e6e19ad0d102dc3d4fa5552cf8b02
Skyfjord <105972841+Skyfjord@users.noreply.github.com>
2023-07-04 15:30:26 +0530

    Add files via upload

---
+++ simplefilter/plothist.py
@@ -0,0 +1,19 @@
+import dataset3 as dataset
+import numpy as np
+import matplotlib.pyplot as plt
+
+alpha=0.8
+bins=dataset.sampleno/20
+tickfreq=20
+
+for i in range(dataset.dim-1,-1,-1):
+    plt.subplot(dataset.dim,1,i+1)
+    plt.ylabel(dataset.dim_names[i])
+    plt.xticks([i/tickfreq for i in range(tickfreq)])
+    plt.hist(np.array(dataset.dataN)[:,i],bins=bins,alpha=alpha,color="red",range=(0,1))
+    plt.hist(np.array(dataset.dataP)[:,i],bins=bins,alpha=alpha,color="green",range=(0,1))
+    
+    # plt.hist(np.array(dataset.dataN)[:,i],bins=bins,alpha=alpha,color="red")
+    # plt.hist(np.array(dataset.dataP)[:,i],bins=bins,alpha=alpha,color="green")
+# plt.title("Analysis of long wave")
+plt.show()
